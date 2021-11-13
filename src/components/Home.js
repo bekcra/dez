8a285ad18f3bcbc0308016af5ce9c7e426b4de14
Alex Craciun <81111659+alexcraciun10@users.noreply.github.com>
2021-11-13 02:08:01 +0200

    init

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Prelungește viața mașinii tale</h1>
+       <h2>folosind piese din dezmembrări</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
